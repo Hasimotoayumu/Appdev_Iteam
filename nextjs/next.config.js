dd2243882baec08b329aa2f6d0b6f3d6963de413
DESKTOP-VU75GP2\hashi <hashimoto2388@outlook.jp>
2024-01-30 12:31:10 +0900

    next.config.jsの中身一部削除

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-  //追加する
-    experimental: {
-      app: true,
-    },
-  
+
   };
   
   module.exports = nextConfig;
