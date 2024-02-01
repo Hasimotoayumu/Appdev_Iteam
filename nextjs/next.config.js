9e6f193ba8c987f4235d630631abb43a5068d22e
橋本　歩武 <131631277+Hasimotoayumu@users.noreply.github.com>
2024-02-01 10:51:54 +0900

    Update next.config.js

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
 
-  };
-  
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  outDir: 'nextjs/out'
+};
+ 
   module.exports = nextConfig;
   
